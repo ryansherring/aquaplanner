c5448eac63e3cd44469a36f5788e55bc9c456ad2
Ryan <ryansherring@gmail.com>
2020-09-07 23:40:37 -0700

    server done before plants

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="appContainer">
+      
+      <div className="navbar" style={{backgroundColor: "red"}}>
+        navbar
+      </div>
+
+      <div style={{backgroundColor: "lightGrey"}}><br /> <br /> <br /> <br />  the rest <br /> </div>
+    
+    <div style={{backgroundColor: "green"}}>Footer</div>
+
+  </div>
   );
 }
 
